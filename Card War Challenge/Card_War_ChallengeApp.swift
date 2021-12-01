3f5f11b84426a7669b958ed3d72017eacb2a91d2
Dawa Zangpo <dzangpo29@gmail.com>
2021-12-01 16:27:05 +1000

    Initial Commit

---
+++ Card War Challenge/Card_War_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card_War_ChallengeApp.swift
+//  Card War Challenge
+//
+//  Created by Dawa Zangpo on 1/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Card_War_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Card War Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Card War Challenge
+//
+//  Created by Dawa Zangpo on 1/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
